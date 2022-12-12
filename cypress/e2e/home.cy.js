4aed03499601ecaa7a421d93716d84763d647e01
Ana Luiza Araujo do Nascimento <ana.nascimento@compasso.com.br>
2022-12-12 11:23:03 -0300

    'home' suit improvement

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,7 +1,12 @@
 describe('home page', () => {
   it('passes', () => {
-    cy.viewport(1440,900) //definir resolução da janela
-    cy.visit('https://buger-eats.vercel.app')
+    //Uma forma de definir resolução da janela:
+    //cy.viewport(1440,900) // mas não precisa pois já foi configurado no config.js
+
+    //Uma forma de definir url inicial:
+    //cy.visit('https://buger-eats.vercel.app') //já foi configurado no config.js
+    //Outra forma de definir url inicial:
+    cy.visit('')
     cy.get('h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
   })
 })
